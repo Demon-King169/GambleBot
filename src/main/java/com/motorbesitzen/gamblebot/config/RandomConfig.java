1ffeb5042baf66b019ff33bab30edf589e7e9657
Kraisie <frolen307@gmail.com>
2021-04-07 20:29:26 +0200

    Made Random Spring controlled to have a single instance instead of creating new ones

---
+++ src/main/java/com/motorbesitzen/gamblebot/config/RandomConfig.java
@@ -0,0 +1,15 @@
+package com.motorbesitzen.gamblebot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class RandomConfig {
+
+	@Bean
+	public Random buildRandom() {
+		return new Random();
+	}
+}
