d2a9ea63255afeb945eb48e2bce44c9d6367b264
Kraisie <frolen307@gmail.com>
2021-04-13 13:52:12 +0200

    Fixed exception on shutdown initiated through Spring

--- src/main/java/com/motorbesitzen/gamblebot/config/EventWaiterConfig.java
+++ src/main/java/com/motorbesitzen/gamblebot/config/EventWaiterConfig.java
@@ -4,12 +4,14 @@ import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.Executors;
+
 @Configuration
 public class EventWaiterConfig {
 
 	@Bean
 	public EventWaiter buildEventWaiter() {
 		// let spring control the EvenWaiter as it is recommended to only use 1 instance
-		return new EventWaiter();
+		return new EventWaiter(Executors.newSingleThreadScheduledExecutor(), false);
 	}
 }
