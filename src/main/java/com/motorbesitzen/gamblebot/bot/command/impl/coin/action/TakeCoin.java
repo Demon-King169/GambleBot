f6a8eca21f4e38bd216d26eca59f351716ebdc1f
Kraisie <frolen307@gmail.com>
2021-04-30 16:32:39 +0200

    Added take command to remove coins from users

---
+++ src/main/java/com/motorbesitzen/gamblebot/bot/command/impl/coin/action/TakeCoin.java
@@ -0,0 +1,104 @@
+package com.motorbesitzen.gamblebot.bot.command.impl.coin.action;
+
+import com.motorbesitzen.gamblebot.bot.command.CommandImpl;
+import com.motorbesitzen.gamblebot.data.dao.DiscordGuild;
+import com.motorbesitzen.gamblebot.data.dao.DiscordMember;
+import com.motorbesitzen.gamblebot.data.repo.DiscordGuildRepo;
+import com.motorbesitzen.gamblebot.data.repo.DiscordMemberRepo;
+import com.motorbesitzen.gamblebot.util.DiscordMessageUtil;
+import com.motorbesitzen.gamblebot.util.LogUtil;
+import com.motorbesitzen.gamblebot.util.ParseUtil;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * Take user coins and delete them.
+ */
+@Service("take")
+class TakeCoin extends CommandImpl {
+
+	private final DiscordMemberRepo memberRepo;
+	private final DiscordGuildRepo guildRepo;
+
+	@Autowired
+	private TakeCoin(final DiscordMemberRepo memberRepo, final DiscordGuildRepo guildRepo) {
+		this.memberRepo = memberRepo;
+		this.guildRepo = guildRepo;
+	}
+
+	@Override
+	public String getName() {
+		return "take";
+	}
+
+	@Override
+	public String getUsage() {
+		return getName() + " (@user|userid) <coins>";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Takes an amount of coins from a given user.";
+	}
+
+	@Override
+	public boolean isAdminCommand() {
+		return true;
+	}
+
+	@Override
+	public boolean isGlobalCommand() {
+		return true;
+	}
+
+	@Override
+	public void execute(final GuildMessageReceivedEvent event) {
+		final Message message = event.getMessage();
+		final long userId = DiscordMessageUtil.getMentionedMemberId(message);
+		if (userId <= 100000000000000L) {
+			sendErrorMessage(event.getChannel(), "That user seems to be invalid!");
+			return;
+		}
+
+		event.getGuild().retrieveMemberById(userId).queue(
+				member -> addCoins(event, member),
+				throwable -> sendErrorMessage(event.getChannel(), "That user is not in this guild!")
+		);
+	}
+
+	private void addCoins(final GuildMessageReceivedEvent event, final Member member) {
+		final long guildId = event.getGuild().getIdLong();
+		final Optional<DiscordMember> dcMemberOpt = memberRepo.findByDiscordIdAndGuild_GuildId(member.getIdLong(), guildId);
+		final DiscordMember dcMember = dcMemberOpt.orElseGet(() -> createNewMember(member.getIdLong(), guildId));
+		final String content = event.getMessage().getContentRaw();
+		final String[] tokens = content.split(" ");
+		final String coinText = tokens[tokens.length - 1];
+		final long coinAmount = ParseUtil.safelyParseStringToLong(coinText);
+		if (coinAmount < 1) {
+			sendErrorMessage(event.getChannel(), "Please set a valid coin amount (> 1)!");
+			return;
+		}
+
+		dcMember.spendCoins(coinAmount);
+		memberRepo.save(dcMember);
+		answer(event.getChannel(), "Took **" + coinAmount + "** coins from the balance of " + member.getAsMention() + ".");
+		LogUtil.logDebug(event.getAuthor().getIdLong() + " took " + coinAmount + " coins from " + dcMember.getDiscordId());
+	}
+
+	private DiscordMember createNewMember(final long memberId, final long guildId) {
+		final Optional<DiscordGuild> dcGuildOpt = guildRepo.findById(guildId);
+		final DiscordGuild dcGuild = dcGuildOpt.orElseGet(() -> createNewGuild(guildId));
+		return DiscordMember.createDefault(memberId, dcGuild);
+	}
+
+	private DiscordGuild createNewGuild(final long guildId) {
+		final DiscordGuild dcGuild = DiscordGuild.withGuildId(guildId);
+		guildRepo.save(dcGuild);
+		return dcGuild;
+	}
+}
