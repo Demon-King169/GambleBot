621969d4b85e90a97f4404d9faed233011a351f9
Kraisie <32745233+Kraisie@users.noreply.github.com>
2021-07-29 06:15:56 +0200

    Add (bad) windows start and stop scripts

---
+++ start.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Starting the bot...
+
+:: Build the project with gradle
+echo    Building bot...
+call gradlew.bat bootJar >nul 2>&1
+
+:: create the environment variables for the bot
+echo    Creating environment...
+for /F %%A in (.env) do SET %%A
+
+:: execute the program with java to avoid gradle overhead
+echo    Executing bot...
+start javaw -jar build\libs\messagewatcher.jar <nul >nul 2>&1
+
+echo Started the bot. The bot will appear online in a few seconds.
---
+++ stop.bat
@@ -0,0 +1,8 @@
+:: THIS FORCES THE PROCESS TO STOP IMMEDIATELY, NO SHUTDOWN HOOKS WILL GET EXECUTED. DATA MAY BE LOST!!!
+@echo off
+echo Stopping the bot forcefully...
+
+:: not quite sure yet how to kill the process not forcefully as removing /F will refuse to kill the process
+:: apparently that's due to the jar running in a console without a window
+for /f "tokens=1" %%i in ('jps -m ^| find "messagewatcher.jar"') do ( taskkill /F /PID %%i )
+echo Stopped the bot (might get shown as online for a few minutes due to forced shutdown).
